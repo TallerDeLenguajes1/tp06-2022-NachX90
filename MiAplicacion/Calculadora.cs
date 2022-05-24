8862ba2a23e21fc60cffe475df24ff97897d4ee6
NachX90 <nachx90@protonmail.com>
2022-05-24 14:48:38 -0300

    .gitignore y Ejercicio 1

---
+++ MiAplicacion/Calculadora.cs
@@ -0,0 +1,28 @@
+﻿public class Calculadora
+{
+    public double Resultado;
+    public Calculadora(double valorInicial)
+    {
+        Resultado = valorInicial;
+    }
+    public void Sumar(double sumando)
+    {
+        Resultado += sumando;
+    }
+    public void Restar(double minuendo)
+    {
+        Resultado -= minuendo;
+    }
+    public void Multiplicar(double termino)
+    {
+        Resultado *= termino;
+    }
+    public void Dividir(double denominador)
+    {
+        Resultado /= denominador;
+    }
+    public void Limpiar()
+    {
+        Resultado = 0;
+    }
+}
---
+++ MiAplicacion/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("Ejercicio 1: Calculadora");
+int seguir, operacion;
+double valorInicial, valorACalcular;
+Calculadora calc = new Calculadora(0);
+Console.Write("Ingrese el valor inicial: ");
+calc.Resultado = double.Parse(Console.ReadLine());
+do
+{
+    do
+    {
+        Console.WriteLine("1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir\n5. Limpiar\n6. Salir");
+        Console.Write("Ingrese la operación a ejecutar: ");
+        operacion = int.Parse(Console.ReadLine());
+    } while (operacion < 1 && operacion > 6);
+    if (operacion < 6)
+    {
+        seguir = 1;
+        Console.Write("Ingrese el valor a operar: ");
+        valorACalcular = double.Parse(Console.ReadLine());
+        switch (operacion)
+        {
+            case 1:
+                calc.Sumar(valorACalcular);
+                break;
+            case 2:
+                calc.Restar(valorACalcular);
+                break;
+            case 3:
+                calc.Multiplicar(valorACalcular);
+                break;
+            case 4:
+                calc.Dividir(valorACalcular);
+                break;
+            case 5:
+                calc.Limpiar();
+                break;
+            default:
+                break;
+        }
+        Console.WriteLine(calc.Resultado);
+    }
+    else
+    {
+        seguir = 0;
+        Console.WriteLine("Gracias por utilizar");
+    }
+} while (seguir == 1);
