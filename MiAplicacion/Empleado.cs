96511d5dd2daa69ba0d17332728e6d87d91d45f5
NachX90 <nachx90@protonmail.com>
2022-06-07 16:53:49 -0300

    Problema 2 terminado

---
+++ MiAplicacion/Empleado.cs
@@ -0,0 +1,71 @@
+﻿public enum Cargos
+{
+    Auxiliar,
+    Administrativo,
+    Ingeniero,
+    Especialista,
+    Investigador
+}
+
+public class Empleado
+{
+    public string? Nombre;
+    public string? Apellido;
+    public DateTime FechaDeNacimiento;
+    public char EstadoCivil;                    // s, c, d, v
+    public char Genero;                         // m, f
+    public DateTime FechaDeIngresoALaEmpresa;
+    public double SueldoBasico;
+    public Cargos Cargo;
+    public int Antiguedad()
+    {
+        DateTime FechaActual = DateTime.Now;
+        if (FechaDeIngresoALaEmpresa.Month >= FechaActual.Month && FechaDeIngresoALaEmpresa.Day > FechaActual.Day)
+        {
+            return (FechaActual.Year - FechaDeIngresoALaEmpresa.Year - 1);
+        }
+        else
+        {
+            return (FechaActual.Year - FechaDeIngresoALaEmpresa.Year);
+        }
+    }
+    public int Edad()
+    {
+        DateTime FechaActual = DateTime.Now;
+        if (FechaDeNacimiento.Month >= FechaActual.Month && FechaDeNacimiento.Day > FechaActual.Day)
+        {
+            return (FechaActual.Year - FechaDeNacimiento.Year - 1);
+        }
+        else
+        {
+            return (FechaActual.Year - FechaDeNacimiento.Year);
+        }
+    }
+    public int AñosParaJubilacion()
+    {
+        int años = Genero == 'm' ? 65 : 60;
+        años -= Edad();
+        return años <= 0 ? 0 : años;
+    }
+    public double Salario()
+    {
+        double Adicional = Antiguedad() < 20 ? (SueldoBasico * 0.01 * Antiguedad()) : (SueldoBasico * 0.25);
+        Adicional *= Cargo == Cargos.Especialista || Cargo == Cargos.Ingeniero ? 1.5 : 1;
+        Adicional += EstadoCivil == 'c' ? 15000 : 0;
+        return SueldoBasico + Adicional;
+    }
+    public void MostrarEmpleado()
+    {
+        Console.WriteLine($"\tApellido y Nombre:\t{Apellido}, {Nombre}");
+        Console.WriteLine($"\tFecha de nacimiento:\t{FechaDeNacimiento.ToShortDateString()}");
+        Console.WriteLine($"\tEstado civil:\t\t{EstadoCivil}");
+        Console.WriteLine($"\tGénero:\t\t\t{Genero}");
+        Console.WriteLine($"\tFecha de ingreso:\t{FechaDeIngresoALaEmpresa.ToShortDateString()}");
+        Console.WriteLine($"\tSueldo Básico:\t\t$ {SueldoBasico}");
+        Console.WriteLine($"\tCargo:\t\t\t{Cargo.ToString()}");
+        Console.WriteLine($"\tAntigüedad:\t\t{Antiguedad()} años");
+        Console.WriteLine($"\tEdad:\t\t\t{Edad()} años");
+        Console.WriteLine($"\tAños para jubilación:\t{AñosParaJubilacion()} años");
+        Console.WriteLine($"\tSalario:\t\t$ {Salario()}");
+    }
+}
--- MiAplicacion/Program.cs
+++ MiAplicacion/Program.cs
@@ -1,47 +1,52 @@
-﻿Console.WriteLine("Ejercicio 1: Calculadora");
-int seguir, operacion;
-double valorInicial, valorACalcular;
-Calculadora calc = new Calculadora(0);
-Console.Write("Ingrese el valor inicial: ");
-calc.Resultado = double.Parse(Console.ReadLine());
-do
+﻿Console.WriteLine("Ejercicio 2: Administración de Personal");
+
+Empleado Empleado1 = new Empleado();
+Empleado1.Nombre = "Juan Manuel";
+Empleado1.Apellido = "Suárez";
+Empleado1.FechaDeNacimiento = new DateTime(1992, 04, 15);
+Empleado1.EstadoCivil = 's';
+Empleado1.Genero = 'm';
+Empleado1.FechaDeIngresoALaEmpresa = new DateTime(2009, 08, 22);
+Empleado1.SueldoBasico = 65000;
+Empleado1.Cargo = Cargos.Ingeniero;
+
+Empleado Empleado2 = new Empleado();
+Empleado2.Nombre = "María Marta";
+Empleado2.Apellido = "López";
+Empleado2.FechaDeNacimiento = new DateTime(1987, 09, 22);
+Empleado2.EstadoCivil = 'c';
+Empleado2.Genero = 'f';
+Empleado2.FechaDeIngresoALaEmpresa = new DateTime(2010, 01, 05);
+Empleado2.SueldoBasico = 65000;
+Empleado2.Cargo = Cargos.Administrativo;
+
+Empleado Empleado3 = new Empleado();
+Empleado3.Nombre = "Fernando Miguel";
+Empleado3.Apellido = "Morales";
+Empleado3.FechaDeNacimiento = new DateTime(1985, 12, 10);
+Empleado3.EstadoCivil = 'c';
+Empleado3.Genero = 'm';
+Empleado3.FechaDeIngresoALaEmpresa = new DateTime(2007, 08, 22);
+Empleado3.SueldoBasico = 65000;
+Empleado3.Cargo = Cargos.Auxiliar;
+
+double TotalDeSalarios = Empleado1.Salario() + Empleado2.Salario() + Empleado3.Salario();
+
+Console.WriteLine($"Monto total de Salarios:\t$ {TotalDeSalarios}");
+
+Console.WriteLine("Empleado más próximo a jubilarse:");
+if (Empleado1.Antiguedad() > Empleado2.Antiguedad() && Empleado1.Antiguedad() > Empleado3.Antiguedad())
 {
-    do
+    Empleado1.MostrarEmpleado();
+}
+else
+{
+    if (Empleado2.Antiguedad() > Empleado3.Antiguedad())
     {
-        Console.WriteLine("1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir\n5. Limpiar\n6. Salir");
-        Console.Write("Ingrese la operación a ejecutar: ");
-        operacion = int.Parse(Console.ReadLine());
-    } while (operacion < 1 && operacion > 6);
-    if (operacion < 6)
-    {
-        seguir = 1;
-        Console.Write("Ingrese el valor a operar: ");
-        valorACalcular = double.Parse(Console.ReadLine());
-        switch (operacion)
-        {
-            case 1:
-                calc.Sumar(valorACalcular);
-                break;
-            case 2:
-                calc.Restar(valorACalcular);
-                break;
-            case 3:
-                calc.Multiplicar(valorACalcular);
-                break;
-            case 4:
-                calc.Dividir(valorACalcular);
-                break;
-            case 5:
-                calc.Limpiar();
-                break;
-            default:
-                break;
-        }
-        Console.WriteLine(calc.Resultado);
+        Empleado2.MostrarEmpleado();
     }
     else
     {
-        seguir = 0;
-        Console.WriteLine("Gracias por utilizar");
+        Empleado3.MostrarEmpleado();
     }
-} while (seguir == 1);
+}
